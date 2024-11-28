e010e76b454f0b9d1ebecc0ae3d018611a01df36
ceofreenet <153985210+ceofreenet@users.noreply.github.com>
2024-11-28 18:44:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "256783678577"
  
-global.botNumber = ["2349019529423"]
+global.botNumber = ["256783678577"]
 
 global.sign = "✘"
 // ☞ ➻ ➽ ☛ ✔
